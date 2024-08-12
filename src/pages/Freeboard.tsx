26852dfc1dfa150c9003d22556f530790d835069
asitis <asitisdev@gmail.com>
2024-08-13 00:56:50 +0900

    feat: 자유게시판 게시글 조회 로딩 스켈레톤 추가

--- src/pages/Freeboard.tsx
+++ src/pages/Freeboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useAuth } from '@/hooks/useAuth';
 
 interface Post {
@@ -16,7 +17,7 @@ interface Post {
 export default function Main() {
   const { postNum } = useParams();
   const { fetchAuth } = useAuth();
-  const [post, setPost] = React.useState<Post>([]);
+  const [post, setPost] = React.useState<Post | null>(null);
 
   React.useEffect(() => {
     const fetchData = async () => {
@@ -31,10 +32,10 @@ export default function Main() {
   return (
     <Card className="w-full max-w-2xl lg:max-w-5xl xl:max-w-7xl">
       <CardHeader>
-        <CardTitle>{post.title}</CardTitle>
-        <CardDescription>{post.user}</CardDescription>
+        <CardTitle>{post ? post.title : <Skeleton className="h-6 w-[400px]" />}</CardTitle>
+        <CardDescription>{post ? post.user : <Skeleton className="h-3.5 w-[100px]" />}</CardDescription>
       </CardHeader>
-      <CardContent>{post.textBody}</CardContent>
+      <CardContent>{post ? post.textBody : <Skeleton className="h-4 w-[300px]" />}</CardContent>
     </Card>
   );
 }
