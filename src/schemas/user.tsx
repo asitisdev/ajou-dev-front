e2761c239c38e57c21efa3f7de3d66ecd2bdf239
asitis <asitisdev@gmail.com>
2024-08-13 14:13:21 +0900

    fix: 회원가입 필드 미입력 오류 메시지 수정

--- src/schemas/user.tsx
+++ src/schemas/user.tsx
@@ -2,16 +2,11 @@ import { z } from 'zod';
 
 const schema = z
   .object({
-    id: z
-      .string()
-      .min(1, {
-        message: '아이디는 필수 입력 항목입니다',
-      })
-      .max(32, {
-        message: '아이디 길이는 최대 32 글자입니다',
-      }),
+    id: z.string({ required_error: '아이디는 필수 입력 항목입니다' }).max(32, {
+      message: '아이디 길이는 최대 32 글자입니다',
+    }),
     password: z
-      .string()
+      .string({ required_error: '비밀번호는 필수 입력 항목입니다' })
       .regex(/^(?=.*[a-zA-Z])/, '비밀번호는 영문자를 포함해야 합니다')
       .regex(/^(?=.*\d)/, '비밀번호는 숫자를 포함해야 합니다')
       .regex(/^(?=.*[\W_])/, '비밀번호는 특수문자를 포함해야 합니다')
@@ -21,16 +16,11 @@ const schema = z
       .max(32, {
         message: '비밀번호 길이는 최대 32 글자입니다',
       }),
-    confirmPassword: z.string().min(1, { message: '비밀번호를 다시 입력해주세요' }),
-    nickname: z
-      .string()
-      .min(1, {
-        message: '닉네임은 필수 입력 항목입니다',
-      })
-      .max(32, {
-        message: '닉네임 길이는 최대 32 글자입니다',
-      }),
-    email: z.string().email({
+    confirmPassword: z.string({ required_error: '비밀번호를 다시 입력해주세요' }),
+    nickname: z.string({ required_error: '닉네임은 필수 입력 항목입니다' }).max(32, {
+      message: '닉네임 길이는 최대 32 글자입니다',
+    }),
+    email: z.string({ required_error: '이메일은 필수 입력 항목입니다' }).email({
       message: '잘못된 이메일 형식입니다',
     }),
   })
