799318b6ff114e2e4aea27b7c2ebdddb6e6290e7
asitis <asitisdev@gmail.com>
2024-08-16 16:20:11 +0900

    feat: 모바일 뷰포트 대응 내비게이션 메뉴 추가

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,16 @@
-import { Outlet, Link, useNavigate } from 'react-router-dom';
-import { CircleUser, Wrench, Search, LogIn } from 'lucide-react';
+import React from 'react';
+import { Outlet, Link, useLocation } from 'react-router-dom';
+import {
+  Menu,
+  CircleUser,
+  Wrench,
+  Search,
+  UserRound,
+  MessageCircle,
+  MessageCircleQuestion,
+  Users,
+  Paperclip,
+} from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -9,42 +20,118 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Sheet, SheetHeader, SheetTitle, SheetDescription, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Input } from '@/components/ui/input';
 import { useAuth } from '@/hooks/useAuth';
+import { cn } from '@/lib/utils';
 
 export default function Root() {
-  const navigate = useNavigate();
+  const location = useLocation();
   const { isAuth, user, logout } = useAuth();
+
+  const [open, setOpen] = React.useState(false);
 
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger className="block md:hidden">
+            <Menu className="h-6 w-6" />
+          </SheetTrigger>
+          <SheetContent side={'left'} className="p-4">
+            <SheetHeader>
+              <SheetTitle className="text-xl">
+                <Wrench className="inline mr-2 h-6 w-6" />
+                <Link to="/" onClick={() => setOpen(false)}>
+                  아주데브
+                </Link>
+              </SheetTitle>
+              <SheetDescription>아주대 개발자 커뮤니티</SheetDescription>
+            </SheetHeader>
+            <nav className="grid items-start py-4">
+              <Link
+                to="/freeboard"
+                className={cn(
+                  'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary',
+                  location.pathname.startsWith('/freeboard') ? 'bg-muted text-primary' : 'text-muted-foreground'
+                )}
+                onClick={() => setOpen(false)}
+              >
+                <MessageCircle className="h-5 w-5" />
+                자유게시판
+              </Link>
+              <Link
+                to="/question"
+                className={cn(
+                  'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary',
+                  location.pathname.startsWith('/question') ? 'bg-muted text-primary' : 'text-muted-foreground'
+                )}
+                onClick={() => setOpen(false)}
+              >
+                <MessageCircleQuestion className="h-5 w-5" />
+                질문게시판
+              </Link>
+              <Link
+                to="collaboration"
+                className={cn(
+                  'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary',
+                  location.pathname.startsWith('/collaboration') ? 'bg-muted text-primary' : 'text-muted-foreground'
+                )}
+                onClick={() => setOpen(false)}
+              >
+                <Users className="h-5 w-5" />
+                구인게시판
+              </Link>
+              <Link
+                to="/resource"
+                className={cn(
+                  'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary',
+                  location.pathname.startsWith('/resource') ? 'bg-muted text-primary' : 'text-muted-foreground'
+                )}
+                onClick={() => setOpen(false)}
+              >
+                <Paperclip className="h-5 w-5" />
+                자료게시판
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+
+        <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+          <Wrench className="h-6 w-6" />
+          <span className="whitespace-nowrap">아주데브</span>
+        </Link>
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-          <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-            <Wrench className="h-6 w-6" />
-            <span className="whitespace-nowrap">아주데브</span>
-          </Link>
           <Link
             to="/freeboard"
             className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap"
           >
             자유게시판
           </Link>
-          <Link to="#" className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap">
+          <Link
+            to="/question"
+            className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap"
+          >
             질문게시판
           </Link>
-          <Link to="#" className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap">
+          <Link
+            to="/collaboration"
+            className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap"
+          >
             구인게시판
           </Link>
-          <Link to="#" className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap">
+          <Link
+            to="/resource"
+            className="text-muted-foreground transition-colors hover:text-foreground whitespace-nowrap"
+          >
             자료게시판
           </Link>
         </nav>
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
           <form className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
+            <div className="relative hidden md:flex">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input type="search" placeholder="검색..." className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]" />
+              <Input type="search" placeholder="검색..." className="pl-8 md:w-[170px] lg:w-[300px]" />
             </div>
           </form>
           {isAuth ? (
@@ -64,10 +151,12 @@ export default function Root() {
               </DropdownMenuContent>
             </DropdownMenu>
           ) : (
-            <Button variant="secondary" onClick={() => navigate('/login')}>
-              <LogIn className="h-5 w-5 mr-2" />
-              <span>로그인</span>
-            </Button>
+            <Link to="/login">
+              <Button variant="secondary">
+                <UserRound className="h-5 w-5" />
+                <span className="ml-2 hidden md:inline">로그인</span>
+              </Button>
+            </Link>
           )}
         </div>
       </header>
